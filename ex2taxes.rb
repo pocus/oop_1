ef561c0ae8684c0f7c331023c5a98d13403486dc
David Yip <dvd.yip@gmail.com>
2014-01-16 15:40:36 -0500

    working after moving inside cartitem

--- ex2taxes.rb
+++ ex2taxes.rb
@@ -12,12 +12,30 @@
 
  class CartItem #could you do a hash to represent each item?
 
- 	def initialize(qty, name, is_imported, is_exempt, price)
- 		@qty = qty
- 		@name = name
- 		@is_imported = is_imported
- 		@is_exempt = is_exempt
- 		@price = price
+ 	def initialize
+ 		@qty = 0
+ 		@name = ""
+ 		@is_imported = false
+ 		@is_exempt = false 
+ 		@price = 0.0
+
+		puts "Input cart item:"
+		keyed_input = gets.chomp #gives a string
+		input_ary = keyed_input.split(" ") #convert to array <spc> delimited
+		
+		@qty = keyed_input[0].to_i
+
+		if input_ary.include?("imported") 
+			@is_imported = true
+		end
+
+		if input_ary.include?("pills") 
+			@is_exempt = true
+		end
+
+		@name = input_ary[1..input_ary.index("at")-1]
+		@price = input_ary[input_ary.index("at") + 1].to_f
+
  	end
 
  	def write
@@ -30,40 +48,9 @@
 
 end
 
+book = CartItem.new
+book.write
 
-
-
-	def get_line
-		qty = 0
-		item_name = ""
-		is_imported = false
-		is_exempt = false
-		price = 0.0
-
-		puts "Input cart item:"
-		keyed_input = gets.chomp #gives a string
-		input_ary = keyed_input.split(" ") #convert to array <spc> delimited
-
-		
-		qty = keyed_input[0].to_i
-
-		if input_ary.include?("imported") 
-			is_imported = true
-		end
-
-		if input_ary.include?("pills") 
-			is_exempt = true
-		end
-
-		name = input_ary[1..input_ary.index("at")-1]
-		price = input_ary[input_ary.index("at") + 1].to_f
-
-		
-		$cartitem = CartItem.new(qty,name,is_imported,is_exempt,price)
-
-
-
-	end
 
 # Taxer.run("CartItem")
 
@@ -91,7 +78,5 @@ end
 
 # #item 1 = CartItem.new(2, book, :imported => true, :exempt => true, 12.49)
 
-get_line
-$cartitem.write
 
 
